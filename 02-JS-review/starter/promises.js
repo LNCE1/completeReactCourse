1f29050075e8690cde60ff56c1c689a87674192d
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2024-08-08 07:12:46 +1000

    Finished Asynchronous JavaScript exercise

--- 02-JS-review/starter/promises.js
+++ 02-JS-review/starter/promises.js
@@ -3,5 +3,10 @@
 //pending: initial state, neither fulfilled nor rejected.
 //fulfilled: meaning that the operation was completed successfully.
 //rejected: meaning that the operation failed.
-fetch("https://jsonplaceholder.typicode.com/todos/1");
-console.log();
+//.then method will be called as soon as the promise is fulfilled. Adding a callback function to conver from Json to a JavaScript object (doing so, returns another object)
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+console.log(
+  "The console will print this line first, because the data for the promise arrived later"
+);
