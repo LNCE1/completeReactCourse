039ed37288b4dd9ad5e84551465e299cde513745
Nicolas Clerici Erhardt <nicolasclericierhardt@gmail.com>
2024-08-08 07:22:13 +1000

    Adding exercise Async Await

--- 02-JS-review/starter/promises.js
+++ 02-JS-review/starter/promises.js
@@ -3,10 +3,20 @@
 //pending: initial state, neither fulfilled nor rejected.
 //fulfilled: meaning that the operation was completed successfully.
 //rejected: meaning that the operation failed.
-//.then method will be called as soon as the promise is fulfilled. Adding a callback function to conver from Json to a JavaScript object (doing so, returns another object)
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
-console.log(
-  "The console will print this line first, because the data for the promise arrived later"
-);
+//.then handlers/method will be called as soon as the promise is fulfilled. Adding a callback function to conver from Json to a JavaScript object (doing so, returns another object)
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+// console.log(
+//   "The console will print this line first, because the data for the promise arrived later"
+// );
+
+//Async/Await. If we await the promise, JS will not move on, by default, to the next line, but wait.
+async function getTodos() {
+  //res = response
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await res.json();
+  console.log(data); //{ userId: 1, id: 1, title: 'delectus aut autem', completed: false }
+  return data;
+}
+const toDos = getTodos();
